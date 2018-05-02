177026cba94859994e3e9f7534a00f9c18050fb5
songeuna <songea812@gmail.com>
2018-05-02 15:17:55 +0900

    1407번

---
+++ 0502/1407.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<math.h>
+
+int f(int *x, int *time)
+{
+	int sqr = 1;
+
+	while((*x)%2 != 1)
+	{
+		*x = *x/2;
+		(*time)++;
+	}
+	
+	for(int i=0; i < *time; i++)
+		sqr = sqr * 2;
+
+	return sqr;
+}
+
+
+void main()
+{	
+	int n = 0;
+	int time,x;
+	int j;
+	
+	printf("자연수 입력 : ");
+	scanf("%d",&n);
+	
+	x = n;
+	
+	j = f(&x, &time);
+
+	printf("f(%d) = %d \n2^%d = %d\n",n,time,time,j);
+}
